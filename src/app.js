cebcabb276115b087349e3418ab95a03755648d7
Ayoze Gómez Rosa <130746651+ayozegr05@users.noreply.github.com>
2023-06-12 18:49:37 +0000

    Terminado

--- src/app.js
+++ src/app.js
@@ -1,11 +1,48 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+  let who = [
+    "The dog",
+    "My grandma",
+    "His turtle",
+    "My bird"
+  ];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+  let action = [
+    "ate",
+    "peed",
+    "crushed",
+    "broke"
+  ];
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+  let what = [
+    "my homework",
+    "the keys",
+    "the car"
+  ];
+
+  let when = [
+    "before the class",
+    "right on time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  // Generamos una excusa aleatoria
+  function generateExcuse() {
+    let randomWho = who[Math.floor(Math.random() * who.length)];
+    let randomaction = action[Math.floor(Math.random() * action.length)];
+    let randomwhat = what[Math.floor(Math.random() * what.length)];
+    let randomwhen = when[Math.floor(Math.random() * when.length)];
+
+    let excuse = randomWho + " " + randomaction + " " + randomwhat + " " + randomwhen + ".";
+    document.getElementById("excuse").textContent = excuse;
+  }
+
+  // Generamos una excusa automáticamente al cargar la página
+  window.addEventListener("load", generateExcuse);
+
+  // Generamos una excusa automáticamente al hacer clic en cualquier botón
+  document.addEventListener("click", generateExcuse);
+
+  // Generamos una excusa automáticamente al pulsar cualquier tecla
+  document.addEventListener("keydown", generateExcuse);
+// }
